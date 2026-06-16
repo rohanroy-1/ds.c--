44bbcb09ed4f264512f02773512a834f6a480b56
Rohan Roy <rohanroy8345@gmail.com>
2026-06-16 11:35:00 +0530

    Add 3loop.cpp

--- 3loop.cpp
+++ 3loop.cpp
@@ -6,7 +6,7 @@ void print1(int n){
     for(int j = 0 ; j<n-i-1 ; j++){
         cout<<" ";}
         //for star
-        for(int j=0 ; j<2*i+1 ; j++ ){
+        for(int j=0 ; j<2*i-1 ; j++ ){
             cout<<"*";}
             //for space 
             for(int j=0 ; j<n-i-1 ; j++){
