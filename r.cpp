24a120709ffaf0bbfc520ede7f45dd170b8991d7
Rohan Roy <rohanroy8345@gmail.com>
2026-07-11 23:57:11 +0530

    building the logic

---
+++ r.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void print1(int n){
+    for(int i=1 ; i<=n ; i++){
+        for(int j=1 ; j<=2n-i+1 ; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0 ; i<t ; i++){
+        int n ; 
+        cin>>n ;
+        print1(n);
+
+    }
+}
